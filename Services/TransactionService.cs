899326f2cee3c3bd3eae0b6a5443c57beac10bbc
Felix Kroener <Felix.Kroener@s.hakmistelbach.ac.at>
2026-05-22 13:15:45 +0200

    seiten angefangen nicht final

---
+++ Services/TransactionService.cs
@@ -0,0 +1,33 @@
+﻿using FinanzAladin.Classes;
+
+namespace Finanzmanager.Services;
+
+public class TransactionService
+{
+    public List<Transaction> Transactions { get; set; } = new();
+
+    public void AddTransaction(Transaction transaction)
+    {
+        transaction.Id = Transactions.Count + 1;
+        Transactions.Add(transaction);
+    }
+
+    public decimal GetIncome()
+    {
+        return Transactions
+            .Where(t => t.Type == TransactionType.Income)
+            .Sum(t => t.Amount);
+    }
+
+    public decimal GetExpenses()
+    {
+        return Transactions
+            .Where(t => t.Type == TransactionType.Expense)
+            .Sum(t => t.Amount);
+    }
+
+    public decimal GetBalance()
+    {
+        return GetIncome() - GetExpenses();
+    }
+}
